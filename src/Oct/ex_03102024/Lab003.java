6ee22e8b7f524d12091fb96c3d57abb96a3fc3ce
Bhavya <bhavyasree2907@gmail.com>
2024-10-15 18:32:01 +0530

    Tasks

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,16 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //Simple Calculator using the SOP statements
+        int a = 50, b = 30;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("Addition =" + sum);
+        System.out.println("Subtraction =" + sub);
+        System.out.println("Multiplication =" + mul);
+        System.out.println("Division =" + div);
+    }
+}
