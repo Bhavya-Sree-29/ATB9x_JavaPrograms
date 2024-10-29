b4cbd414a9eb2f25a5ab59bfc81d4369e61e9043
Bhavya <bhavyasree2907@gmail.com>
2024-10-29 12:19:48 +0530

    Tasks

--- src/Oct/ex_15102024/Lab015.java
+++ src/Oct/ex_15102024/Lab015.java
@@ -2,9 +2,8 @@ package Oct.ex_15102024;
 
 public class Lab015 {
     public static void main(String[] args) {
-        int a = 10, b = 25, c = 15, max;
-       // max = ((a>b))? (((b>c)? b : c) : ((a>c)? a : c));
-        max = ((a>b) ? a : (a>c)? a : (b>c)? b : c);
+        int a = 100, b = 5, c = 155, max;
+        max = ((a>b) && (a>c))? a : (((b>a) && (b>c))? b : c );
         System.out.println(max);
 
     }
