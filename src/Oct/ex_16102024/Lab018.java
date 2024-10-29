3c0ce325f38d19f59cc105d8510150426a68a4f8
Bhavya <bhavyasree2907@gmail.com>
2024-10-29 12:58:14 +0530

    Tasks

--- src/Oct/ex_16102024/Lab018.java
+++ src/Oct/ex_16102024/Lab018.java
@@ -1,4 +1,8 @@
 package Oct.ex_16102024;
 
 public class Lab018 {
+    public static void main(String[] args) {
+        int age = Integer.parseInt(args[0]);
+        System.out.println((age>25) ? "Allowed to go to vacation" : "Not Allowed");
+    }
 }
