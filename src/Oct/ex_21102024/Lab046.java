63db329aec2ee98c071afba43f832703f79c5022
Bhavya <bhavyasree2907@gmail.com>
2024-11-12 13:12:05 +0530

    Tasks

---
+++ src/Oct/ex_21102024/Lab046.java
@@ -0,0 +1,12 @@
+package Oct.ex_21102024;
+
+public class Lab046 {
+    public static void main(String[] args) {
+        int i = 10;
+        for (; i > 0; i--) {
+            System.out.println(i);
+
+        }
+    }
+}
+
---
+++ src/Oct/ex_21102024/Lab047.java
@@ -0,0 +1,13 @@
+package Oct.ex_21102024;
+
+public class Lab047 {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        for (int i = 0; i < 10 ;i++) { // Times - 10 -> Pramod
+            System.out.println("Pramod");
+        }
+
+        System.out.println("Outside Loop");
+    }
+}
