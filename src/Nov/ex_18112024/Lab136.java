92f269f4a99f0a91627166d320df80adac65b75d
Bhavya <bhavyasree2907@gmail.com>
2024-12-16 19:29:49 +0530

    Tasks

--- src/Nov/ex_18112024/Lab136.java
+++ src/Nov/ex_18112024/Lab136.java
@@ -2,6 +2,7 @@ package Nov.ex_18112024;
 
 public class Lab136 {
     public static void main(String[] args) {
+        //System.exit(0); means where finally is not executed
         try {
             int a = 10/10;
             //System.exit(0);
